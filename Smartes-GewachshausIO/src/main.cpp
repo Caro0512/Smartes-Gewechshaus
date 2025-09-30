de0a2942e398d715b6307e3c4e06558e7a340c15
Patrik <Patrik.schwarz@htlpinkafeld.at>
2025-09-30 09:57:57 +0200

    30.09.2025 upadted version

--- Smartes-GewachshausIO/src/main.cpp
+++ Smartes-GewachshausIO/src/main.cpp
@@ -5,11 +5,14 @@ int myFunction(int, int);
 
 void setup() {
   // put your setup code here, to run once:
+   Serial.begin(115200); 
   int result = myFunction(2, 3);
+  Serial.println(result);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  printf("Result: %d\n", myFunction(5, 7));
 }
 
 // put function definitions here:
