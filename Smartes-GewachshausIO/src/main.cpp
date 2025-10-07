fb76de69dae7e38341fa404c14fcbecdb5e4adb8
Patrik <Patrik.schwarz@htlpinkafeld.at>
2025-10-07 08:57:05 +0200

    7.10.2025 updated version: 2 Sensoren funktionsfähig

--- Smartes-GewachshausIO/src/main.cpp
+++ Smartes-GewachshausIO/src/main.cpp
@@ -1,15 +1,42 @@
 #include <Arduino.h>
+#include "DHT.h"
 
-const int sensor = 34;
+// --- Pin-Definitionen ---
+const int BODEN_SENSOR_PIN = 34; 
+#define DHTPIN 18                
+#define DHTTYPE DHT11
+
+// --- Sensor-Initialisierung ---
+DHT dht(DHTPIN, DHTTYPE);
 
 void setup() {
   Serial.begin(115200); 
-  Serial.println("Ausgabe funktionsfähig:");
+  Serial.println("Kombinierter Sensor-Test gestartet!");
+  
+  dht.begin(); 
 }
 
 void loop() {
-  int sensorValue = analogRead(sensor);
-  Serial.println(sensorValue);
   
-  delay(500); 
+  int bodenWert = analogRead(BODEN_SENSOR_PIN);
+
+
+  float luftfeuchte = dht.readHumidity();
+
+
+  Serial.print("Bodenfeuchtigkeit (Rohwert): ");
+  Serial.println(bodenWert);
+
+ 
+  if (isnan(luftfeuchte)) {
+    Serial.println("Fehler beim Auslesen des DHT11 Sensors!");
+  } else {
+    Serial.print("Luftfeuchtigkeit: ");
+    Serial.print(luftfeuchte);
+    Serial.println("%");
+  }
+  Serial.println("------------------------------------");
+
+
+  delay(2000);
 }
